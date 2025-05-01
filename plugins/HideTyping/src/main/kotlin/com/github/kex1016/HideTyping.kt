a6e2a6b062b563cabff2653cb3dcb8f50ddb0c4f
Kex1016 <cakes@haiiro.moe>
2025-05-01 22:14:27 +0200

    class name...

--- plugins/HideTyping/src/main/kotlin/com/github/kex1016/HideTyping.kt
+++ plugins/HideTyping/src/main/kotlin/com/github/kex1016/HideTyping.kt
@@ -2,18 +2,12 @@ package com.github.kex1016
 
 import android.content.Context
 import com.aliucord.annotations.AliucordPlugin
-import com.aliucord.entities.MessageEmbedBuilder
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.*
-import com.aliucord.wrappers.embeds.MessageEmbedWrapper.Companion.title
-import com.discord.models.user.CoreUser
 import com.discord.stores.StoreUserTyping
-import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
-import com.discord.widgets.chat.list.entries.ChatListEntry
-import com.discord.widgets.chat.list.entries.MessageEntry
 
 @AliucordPlugin(requiresRestart = false)
-class MyFirstPatch : Plugin() {
+class HideTyping : Plugin() {
     override fun start(context: Context) {
         patcher.instead<StoreUserTyping>(
             "setUserTyping", Long::class.java // long channelId
